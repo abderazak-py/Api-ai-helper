96519bdf2a587c66d81f06033d0b698dd1fd5d8b
Abderazak Achour <71965021+abderazak-py@users.noreply.github.com>
2025-12-08 14:30:49 +0100

    added api endpoint posts

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/posts', function () {
+    return ['id' => '1', 'content' => 'this is my first post'];
+});
+
+Route::prefix('v1')->group(function () {Route::apiResource('posts', PostController::class);
+    }
+);
